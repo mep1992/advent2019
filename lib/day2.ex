cf033c05c4fc5995939b06dc352edc31b8d28b4b
Michael Parker <me.parker1992@gmail.com>
2020-08-14 14:46:41 +0200

    day1, part2: complete part 2, refactor

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,14 +1,35 @@
 defmodule Day2 do
   def part1() do
-    module_masses = File.read!("lib/day2_input.txt")
-
-    String.split(module_masses, ",")
-    |> Enum.map(&String.to_integer(&1))
+    get_memory()
     |> List.replace_at(1, 12)
     |> List.replace_at(2, 2)
     |> IntcodeComputer.run()
+  end
+
+  def part2() do
+    memory = get_memory()
+
+    inputs = for noun <- 0..99, verb <- 0..99, do: {noun, verb}
+
+    Enum.find(
+      inputs,
+      fn {noun, verb} ->
+        memory
+        |> List.replace_at(1, noun)
+        |> List.replace_at(2, verb)
+        |> IntcodeComputer.run()
+        |> List.first() == 19_690_720
+      end
+    )
+  end
+
+  defp get_memory do
+    File.read!("lib/day2_input.txt")
+    |> String.split(",")
+    |> Enum.map(&String.to_integer(&1))
   end
 end
 
 IO.puts("Day2 Output")
 IO.inspect(Day2.part1(), label: "part1", limit: :infinity)
+IO.inspect(Day2.part2(), label: "part2")
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -5,4 +5,8 @@ defmodule Day2Test do
   test "calculates value at position 0 correctly" do
     assert Day2.part1() |> List.first() == 4_462_686
   end
+
+  test "calculates noun and verb correctly" do
+    assert Day2.part2() |> (fn {noun, verb} -> 100 * noun + verb end).() == 5936
+  end
 end
